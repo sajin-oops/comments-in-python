14a9f313d902b769169e91d31d7d4ae47b8cfed2
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-07-06 17:19:21 +0530

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,8 @@
+# this is single line comment
+print("hello world")
+
+'''
+this is multi line comments
+what ever you write inside this that will not considered as a code 
+its just for understanding for the developer like me :)
+'''
